bde6fa7be791077c087a7c93a144d5b15b02579d
Patrick van den Berg <pvandenberg@schubergphilis.com>
2016-11-11 21:18:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# mySecunia
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'mySecunia'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures mySecunia'
+long_description 'Installs/Configures mySecunia'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/mySecunia/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/mySecunia' if respond_to?(:source_url)
